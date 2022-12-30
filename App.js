3b1034a8a9d48d65c5ff77c33ecd9bca0900f7f6
Ricardo Santana <ricardoesantana@gmail.com>
2022-12-30 03:14:37 -0300

    Atividade 02 - React Native (Mobile)

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
+import { React } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Avatar, Text } from 'react-native-elements';
+import { Icon } from 'react-native-vector-icons/FontAwesome';
 
+const Flex = () => {
+  return (
+    <View style={[styles.container, {
+      flexDirection: "column"
+    }]}>
+      <View style={{ flex: 2, alignItems:'center', paddingTop: 30 }} >
+        <Avatar size="xlarge" rounded 
+          source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR71EGQo_g27b5xIMiBLPUe8lvKv572RublFA&usqp=CAU" }}
+        />
+        <Text h4>Vacinação Digital</Text>
+      </View>
+      <View style={{ flex: 3 }} >
+        <Text h4>Email</Text> 
+        <Input placeholder='' />
+        <Text h4>Senha</Text>
+        <Input placeholder='' />
+        <Text> </Text>
+        <Button title="Logar" type="solid" />
+        <Text> </Text>
+        <Button title="Cadastre-se" type="solid" />
+      </View>
+    </View>);
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00FFFF',
+    padding: 20,
+  },
+});
+
+export default Flex;
+
+/*
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Input placeholder='Login' />
+        <Input placeholder='Senha' />
+      </View>
+      <Button title="Logar" type="solid" />
     </View>
   );
 }
@@ -18,3 +59,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
+
